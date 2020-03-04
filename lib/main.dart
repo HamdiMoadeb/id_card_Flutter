82e75f98d1e79857a6be852558c68f2075baa203
Hamdi Moadeb <hamdimoadeb@gmail.com>
2020-03-04 10:32:10 +0100

    Using Container and Colums/Rows

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            verticalDirection: VerticalDirection.up,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 120.0,
+                color: Colors.teal,
+                child: Text('Container 1'),
+              ),
+              Container(
+                height: 100.0,
+                width: 120.0,
+                color: Colors.blue,
+                child: Text('Container 2'),
+              ),
+              Container(
+                height: 100.0,
+                width: 120.0,
+                color: Colors.red,
+                child: Text('Container 3'),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
